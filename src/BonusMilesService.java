06a94b8cff77f9171c738be59c895c8784dc6a71
Nikolay Dementev <dementevni@mail.ru>
2023-01-23 11:30:53 +0500

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,13 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int result = 500;
+        boolean inRegistered = true;
+        int miles;
+        if (inRegistered) {
+            miles = 1;
+        } else {
+            miles = 0;
+        }
+        return result;
+    }
+}
