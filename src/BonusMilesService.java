e5289f4d3b570e59befafca1a4969655635b2722
Nikolay Dementev <dementevni@mail.ru>
2023-01-26 11:27:02 +0500

    Main changes

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,13 +1,5 @@
 public class BonusMilesService {
-    public int calculate(int price) {
-        int result = 500;
-        boolean inRegistered = true;
-        int miles;
-        if (inRegistered) {
-            miles = 1;
-        } else {
-            miles = 0;
-        }
-        return result;
+    public int calculate(int cost) {
+        return cost / 20;
     }
 }
